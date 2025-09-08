fd9b8f321092cfd0fd54b5b84d3607bb5385b691
Arne Halvorsen <arne.halvorsen@gmail.com>
2025-09-08 07:25:20 +0200

    added missing loging

--- src/astrogeist/engine/scanner/userdata/UserDataScanner.java
+++ src/astrogeist/engine/scanner/userdata/UserDataScanner.java
@@ -1,31 +1,39 @@
 package astrogeist.engine.scanner.userdata;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import astrogeist.engine.abstraction.Scanner;
 import astrogeist.engine.abstraction.Timeline;
 import astrogeist.engine.abstraction.TimelineValuePool;
+import astrogeist.engine.logging.Log;
 import astrogeist.engine.resources.Resources;
 import astrogeist.engine.userdata.UserDataIo;
 import astrogeist.engine.util.FilesUtil;
 import astrogeist.engine.util.Instants;
 
 public final class UserDataScanner implements Scanner {
+	private final Logger logger = Log.get(this);
+	
 	private final UserDataIo userDataIo;
 	
-	public UserDataScanner(TimelineValuePool tvp) {
-		this.userDataIo = new UserDataIo(tvp); }
+	public UserDataScanner(TimelineValuePool tvp) { this.userDataIo = new UserDataIo(tvp); }
 
-	@Override public void scan(Timeline timeline) {
+	@Override public final void scan(Timeline timeline) {
 		try {
 			var root = Resources.getUserDataDir();
 			var files = root.listFiles();
 			for (var file : files) {
+				
+				this.logger.info("analyze path: " + file.toString());
+				
 				var name = FilesUtil.getBaseName(file);
 				var t = Instants.fromFileSafeString(name);
 				var userData = this.userDataIo.load(t);
 				timeline.putTimelineValues(t, userData);
 			}
 		} catch (Exception x) {
-			System.err.println(x.getLocalizedMessage());
+			this.logger.log(Level.SEVERE, "Failed analyzing", x);
 		}
 	}
 

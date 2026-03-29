352461d0227d432328c1fe5a6c6ba4b6628cac26
Arne Halvorsen <arne.halvorsen@gmail.com>
2026-03-29 14:02:41 +0200

    wip on moving common code to desktop project

--- src/aha/common/guard/AAMovedToAhaDesktop.java
+++
@@ -1,7 +0,0 @@
-package aha.common.guard;
-
-import static aha.common.guard.ObjectGuards.throwStaticClassInstantiateError;
-
-public final class AAMovedToAhaDesktop {
-	private AAMovedToAhaDesktop() { throwStaticClassInstantiateError(); }
-}
